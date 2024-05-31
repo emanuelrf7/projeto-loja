31d6f27c4eab33ccc57b50306ccf6d8e6613fcb3
Emanuel Rosa Franco <139822686+emanuelrf7@users.noreply.github.com>
2024-05-30 22:31:25 -0300

    Desenvolvi o script do projeto

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 // Seleção de Elementos
-
+const botaoAdc = document.querySelector('input#adicionar')
+const nomeEl = document.querySelector('input#inome')
+const tamanhoEl = document.querySelector('input#itamanho')
+const descricaoEl = document.querySelector('input#idesc')
+const valorEl = document.querySelector('input#ivalor')
+const produtos = document.querySelector('div#produtos')
 // Funções
+function adicionarProduto() {
+    // Criação de elementos
+    const novoEl = document.createElement('div');
+    const textoNome = document.createElement('p');
+    const textoTamanho = document.createElement('p');
+    const textoDescricao = document.createElement('p');
+    const textoValor = document.createElement('p');
+    const img = document.createElement('img');
 
-// Código principal
+    // Manipulação de elementos
+    const nome = nomeEl.value;
+    const tamanho = tamanhoEl.value;        
+    const descricao = descricaoEl.value;        
+    const valor = valorEl.value;
+    textoNome.innerText = `Nome da roupa: ${nome}`;
+    textoTamanho.innerText = `Tamanho: ${tamanho}`;
+    textoDescricao.innerText = `Descricao: ${descricao}`;
+    textoValor.innerText = `Valor: R$${valor}`;
+    img.src = prompt('Digite o link da imagem: ');
+    img.alt = 'Foto do Produto';
+    novoEl.append(img, textoNome, textoTamanho, textoDescricao, textoValor);
+    produtos.appendChild(novoEl);
+    console.log(novoEl);
+    
+    // Reset do formulário
+    nomeEl.value = '';
+    tamanhoEl.value = '';
+    descricaoEl.value = '';
+    valorEl.value = '';
+}
